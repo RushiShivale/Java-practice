5f2a5a51764e6917f07cbc624bd7cec2e18a6d5a
RushiShivale <rushikeshshivale38@gmail.com>
2026-03-12 23:23:17 +0530

    max sub array (brute force)

---
+++ Array/maxSubArr.java
@@ -0,0 +1,28 @@
+
+public class maxSubArr {
+    public static int maxSubArr(int number[]){
+        int max_value = Integer.MIN_VALUE;
+
+        for (int i = 0; i < number.length; i++){
+            for (int j = i; j<number.length;j++){
+                int curr = 0;
+                for (int k = j; k<number.length; k++){
+                    
+                    curr += number[k];
+                }
+                if (max_value < curr){
+                    max_value = curr;
+                }
+               
+            }
+        }
+        return max_value;
+    }
+
+    public static void main (String args[]){
+        int number[] = {12,13,34,65,23};
+
+        System.out.println("the Max sub array addition is : "+maxSubArr(number));
+
+    }
+}
